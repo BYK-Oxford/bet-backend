bb7d938e511839be40a9c2274a7b506ac92b1292
Gurung-Manish <manishgrg496@gmail.com>
2025-02-25 11:18:21 +0000

    updated orm class for standing model.

--- app/standings/models/standings_model.py
+++ app/standings/models/standings_model.py
@@ -7,9 +7,18 @@ class Standing(Base):
 
     standing_id = Column(String, primary_key=True, index=True)
     team_id = Column(String, ForeignKey("teams.team_id"), nullable=False)
-    ranking = Column(Integer, nullable=False)
     league_id = Column(String, ForeignKey("leagues.league_id"), nullable=False)
     season_id = Column(String, ForeignKey("seasons.season_id"), nullable=False)
+
+    position = Column(Integer, nullable=False)  # Renamed from ranking
+    played = Column(Integer, nullable=False)
+    wins = Column(Integer, nullable=False)
+    draws = Column(Integer, nullable=False)
+    losses = Column(Integer, nullable=False)
+    goals_for = Column(Integer, nullable=False)
+    goals_against = Column(Integer, nullable=False)
+    goal_difference = Column(Integer, nullable=False)
+    points = Column(Integer, nullable=False)
 
     team = relationship("Team", back_populates="standings")
     league = relationship("League", back_populates="standings")
