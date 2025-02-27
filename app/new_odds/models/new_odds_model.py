bd2b8936e963731328c4ff22f68b322a9cb4eb53
Gurung-Manish <manishgrg496@gmail.com>
2025-02-27 11:53:49 +0000

    made new orm for new odds and current league

---
+++ app/new_odds/models/new_odds_model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, String, ForeignKey, Integer, DateTime, Float, Time
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+class NewOdds(Base):
+    __tablename__ = "new_odds"
+
+    new_odds_id = Column(String, primary_key=True, index=True)
+    date = Column(DateTime, nullable=False)
+    time = Column(Time, nullable=False)
+
+    home_team_id = Column(String, ForeignKey("teams.team_id"), nullable=False)
+    away_team_id = Column(String, ForeignKey("teams.team_id"), nullable=False)
+    
+    home_odds = Column(Float)
+    draw_odds = Column(Float)
+    away_odds = Column(Float)
+    
+    home_team = relationship("Team", foreign_keys=[home_team_id], back_populates="home_matches")
+    away_team = relationship("Team", foreign_keys=[away_team_id], back_populates="away_matches")
