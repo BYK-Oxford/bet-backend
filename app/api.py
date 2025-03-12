6718970d6ec5d076d9642187f4f2e147ec47747a
Gurung-Manish <manishgrg496@gmail.com>
2025-03-12 16:00:56 +0000

    updating api

--- app/api.py
+++ app/api.py
@@ -6,18 +6,8 @@ from app.new_odds.controllers.new_odds_controller import router as new_odds_rout
 from app.odds_calculation.controllers.odds_calculation_controller import router as odds_calculation_router
 
 api_router = APIRouter()
-
 api_router.include_router(upload_match_router, prefix="/match", tags=["match"])
-
-
 api_router.include_router(upload_standing_router, prefix="/standing", tags=["standing"])
-
-
 api_router.include_router(current_league_router, prefix="/current-league", tags=["current-league"])
-
-
 api_router.include_router(new_odds_router, prefix="/odds", tags=["odds"])
-
-
-
 api_router.include_router(odds_calculation_router, prefix="/odds-calculation", tags=["odds-calculation"])
