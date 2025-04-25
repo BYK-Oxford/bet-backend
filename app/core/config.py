d83c1c0128152ae79a75aced2639cb60760c1b85
Gurung-Manish <manishgrg496@gmail.com>
2025-04-25 18:10:45 +0100

    change cred

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,6 @@ import os
 load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:B11rry2025**@db.zgjotpgtzqiqimolgper.supabase.co:5432/postgres"
+    DATABASE_URL: str = "postgresql://postgres:B11rry2025**@db.zgjotpgtzqiqimolgper.supabase.co:5432/postgres?sslmode=require"
 
 settings = Settings()
