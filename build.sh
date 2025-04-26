2e730abc3f68ba4ac76995880b1bc6fe3eddf1fe
Gurung-Manish <manishgrg496@gmail.com>
2025-04-26 10:08:03 +0100

    check driver location

--- build.sh
+++ build.sh
@@ -15,5 +15,25 @@ apt-get install -y \
     libxtst6 \
     xdg-utils
 
+
+# Check if chromedriver is installed and print its location
+CHROMEDRIVER_PATH=$(which chromedriver)
+
+if [ -z "$CHROMEDRIVER_PATH" ]; then
+    echo "Chromedriver is not installed."
+else
+    echo "Chromedriver is installed at: $CHROMEDRIVER_PATH"
+fi
+
+# Check if chromium is installed and print its location
+CHROMIUM_PATH=$(which chromium)
+
+if [ -z "$CHROMIUM_PATH" ]; then
+    echo "Chromium is not installed."
+else
+    echo "Chromium is installed at: $CHROMIUM_PATH"
+fi
+
+
 # Install other Python dependencies
 pip install -r requirements.txt
