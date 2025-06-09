8704c1553002ae83ea19a5d24816d93e39185392
Gurung-Manish <manishgrg496@gmail.com>
2025-06-09 12:23:58 +0100

    calling the function in betfair controller

--- app/new_odds/controllers/betfair_odds_controller.py
+++ app/new_odds/controllers/betfair_odds_controller.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.new_odds.services.betfair_service import get_betfair_odds
 import asyncio
-import json
 
 router = APIRouter()
-
 
 @router.post("/betfair-odds/")
 async def get_new_betfair_odds(db: Session = Depends(get_db)):
     """Trigger getting new betfair odds"""
-    print(f"🔵Getting new odds from BetFair")
+    print(f"🔵 Getting new odds from BetFair")
     
-    
+    # Run get_betfair_odds() in a separate thread (non-blocking)
+    await asyncio.to_thread(get_betfair_odds)
+
+    return {"message": "Triggered fetching Betfair odds"}
